df77ce1ba2828d64087d1cf2864aba13cada70c5
Rytis Mitkus <77438479+RytisMitkus@users.noreply.github.com>
2022-07-29 21:46:12 +0300

    env

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,8 +2,10 @@ import {
 	ApolloClient, InMemoryCache
 } from "@apollo/client"
 // https://express-typescript-boilerplate-production.up.railway.app/graphql
+
+const db = process.env.ENV === 'dev' ? 'http://localhost:5050/graphql' : 'https://express-typescript-boilerplate-production.up.railway.app/graphql'
 const client = new ApolloClient({
-	uri: "http://localhost:5050/graphql",
+	uri: db,
 	cache: new InMemoryCache(),
 })
 
